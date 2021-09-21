23b6cd69b1788be8cd4ec0fdc156169d2e637ae1
Daniel Stuhr Petersen <d@stuhrs.dk>
2021-09-21 19:07:06 +0200

    fixed indentation

--- configuration.h
+++ configuration.h
@@ -4,8 +4,8 @@
 
 struct config {
 	char mail_recipient[50];
-  char temperature_source_file[50];
-  int temperature_read_count;
+	char temperature_source_file[50];
+	int temperature_read_count;
 	int interval_seconds;
 	double temperature_threshold;
 };
@@ -18,11 +18,12 @@ struct config parse_args(int count, char *arguments[]) {
 	}
 	struct config config;
 
-  config.temperature_read_count = TEMPERATURE_COUNT;
+	config.temperature_read_count = TEMPERATURE_COUNT;
 	config.temperature_threshold = atof(arguments[1]);
 	config.interval_seconds = atoi(arguments[2]);
 	strncpy(config.mail_recipient, arguments[3], sizeof(config.mail_recipient));
-  strncpy(config.temperature_source_file, TEMPERATURE_FILE, sizeof(config.temperature_source_file));
+	strncpy(config.temperature_source_file, TEMPERATURE_FILE,
+	sizeof(config.temperature_source_file));
 
 	return config;
 }
